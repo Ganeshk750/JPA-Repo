836460cead86cf45b874dd6e30679e8fb75a44cc
Ganeshk750 <ganesh9ot@gmail.com>
2020-06-11 00:06:30 +0530

    adding annotation in repo

--- one-to-one/src/main/java/com/ganesh/InstructorRepository.java
+++ one-to-one/src/main/java/com/ganesh/InstructorRepository.java
@@ -1,9 +1,11 @@
 package com.ganesh;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ganesh.entity.Instructor;
 
+@Repository
 public interface InstructorRepository extends JpaRepository<Instructor,  Long>{
 
 }
