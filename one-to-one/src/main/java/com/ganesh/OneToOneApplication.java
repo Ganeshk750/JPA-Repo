98a597cfafd79eff33d51f43df6edcbe74bd2b4b
Ganeshk750 <ganesh9ot@gmail.com>
2020-06-11 00:34:49 +0530

    adding controller and exception class

--- one-to-one/src/main/java/com/ganesh/OneToOneApplication.java
+++ one-to-one/src/main/java/com/ganesh/OneToOneApplication.java
@@ -1,13 +1,51 @@
 package com.ganesh;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ganesh.entity.Instructor;
+import com.ganesh.entity.InstructorDetail;
+import com.ganesh.repo.InstructorRepository;
+
 @SpringBootApplication
-public class OneToOneApplication {
+public class OneToOneApplication implements CommandLineRunner {
+	
+	
+    private InstructorRepository instructorRepository;
+    
+    @Autowired
+    public OneToOneApplication(InstructorRepository instructorRepository) {
+	  this.instructorRepository = instructorRepository;	
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(OneToOneApplication.class, args);
 	}
+	
+	
+	 
+	    @Override
+	    public void run(String...args) throws Exception {
+
+	        Instructor instructor = new Instructor("Ramesh", "Sir", "ramesh@gmail.com");
+
+	        InstructorDetail instructorDetail = new InstructorDetail("Java Ist", "Cricket Playing");
+	        
+	        Instructor instructor1 = new Instructor("Ramesh", "Sir", "ramesh@gmail.com");
+	        InstructorDetail instructorDetails = new InstructorDetail("Angular", "Cricket Playing");
+
+	        // associate the objects
+	        instructor.setInstructorDetails(instructorDetail);
+
+	        instructorRepository.save(instructor);
+	        
+	        // associate the objects 
+	        instructor1.setInstructorDetails(instructorDetails);
+
+	        instructorRepository.save(instructor1);
+	    }
 
 }
--- one-to-one/src/main/java/com/ganesh/controller/InstructorController.java
+++ one-to-one/src/main/java/com/ganesh/controller/InstructorController.java
@@ -1,5 +1,78 @@
 package com.ganesh.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ganesh.entity.Instructor;
+import com.ganesh.exception.ResourceNotFoundException;
+import com.ganesh.repo.InstructorRepository;
+
+@RestController
+@RequestMapping("/api/v1")
 public class InstructorController {
+	
+	private InstructorRepository instructorRepository;
+	
+	@Autowired
+	public InstructorController(InstructorRepository instructorRepository) {
+		this.instructorRepository = instructorRepository;
+	}
+	
+	@GetMapping("/instructors")
+    public List < Instructor > getInstructors() {
+        return instructorRepository.findAll();
+    }
+
+    @GetMapping("/instructors/{id}")
+    public ResponseEntity < Instructor > getInstructorById(
+        @PathVariable(value = "id") Long instructorId) throws ResourceNotFoundException {
+        Instructor user = instructorRepository.findById(instructorId)
+            .orElseThrow(() -> new ResourceNotFoundException("Instructor not found :: " + instructorId));
+        return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping("/instructors")
+    public Instructor createUser(@Validated @RequestBody Instructor instructor) {
+        return instructorRepository.save(instructor);
+    }
+
+    @PutMapping("/instructors/{id}")
+    public ResponseEntity < Instructor > updateUser(
+        @PathVariable(value = "id") Long instructorId,
+        @RequestBody Instructor userDetails) throws ResourceNotFoundException {
+        Instructor user = instructorRepository.findById(instructorId)
+            .orElseThrow(() -> new ResourceNotFoundException("Instructor not found :: " + instructorId));
+        user.setEmail(userDetails.getEmail());
+        final Instructor updatedUser = instructorRepository.save(user);
+        return ResponseEntity.ok(updatedUser);
+    }
+    
+    
+    @DeleteMapping("/instructors/{id}")
+    public Map < String, Boolean > deleteUser(
+        @PathVariable(value = "id") Long instructorId) throws ResourceNotFoundException {
+        Instructor instructor = instructorRepository.findById(instructorId)
+            .orElseThrow(() -> new ResourceNotFoundException("Instructor not found :: " + instructorId));
+
+        instructorRepository.delete(instructor);
+        Map < String, Boolean > response = new HashMap < > ();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
+
 
 }
