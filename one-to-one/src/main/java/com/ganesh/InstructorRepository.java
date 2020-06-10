3a5a51758e4fc53483db6b388e6ad056b4e8357b
Ganeshk750 <ganesh9ot@gmail.com>
2020-06-11 00:05:18 +0530

    adding repo

--- one-to-one/src/main/java/com/ganesh/InstructorRepository.java
+++ one-to-one/src/main/java/com/ganesh/InstructorRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ganesh.entity.Instructor;
 
-public interface InstructorRepository extends JpaRepository<Instructor, Long>{
+public interface InstructorRepository extends JpaRepository<Instructor,  Long>{
 
 }
