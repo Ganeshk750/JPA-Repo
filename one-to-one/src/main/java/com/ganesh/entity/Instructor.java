2c92932c092a6884d78ef575b4bfc9cb2305498c
Ganeshk750 <ganesh9ot@gmail.com>
2020-06-10 23:57:19 +0530

    adding entity and establish relations b/w two class

---
+++ one-to-one/src/main/java/com/ganesh/entity/Instructor.java
@@ -0,0 +1,78 @@
+package com.ganesh.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Instructor {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	private String fristName;
+	
+	private String lastName;
+	
+	private String email;
+	
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "instructor_detail_id")
+	private InstructorDetail instructorDetails;
+	
+
+	public Instructor(String fristName, String lastName, String email) {
+		this.fristName = fristName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFristName() {
+		return fristName;
+	}
+
+	public void setFristName(String fristName) {
+		this.fristName = fristName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public InstructorDetail getInstructorDetails() {
+		return instructorDetails;
+	}
+
+	public void setInstructorDetails(InstructorDetail instructorDetails) {
+		this.instructorDetails = instructorDetails;
+	}
+	
+	
+
+}
---
+++ one-to-one/src/main/java/com/ganesh/entity/InstructorDetail.java
@@ -0,0 +1,49 @@
+package com.ganesh.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class InstructorDetail {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String channelName;
+	private String hobby;
+	
+	public InstructorDetail() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public InstructorDetail(String channelName, String hobby) {
+		this.channelName = channelName;
+		this.hobby = hobby;
+	}
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getChannelName() {
+		return channelName;
+	}
+	public void setChannelName(String channelName) {
+		this.channelName = channelName;
+	}
+	public String getHobby() {
+		return hobby;
+	}
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+	
+	
+	
+
+}
