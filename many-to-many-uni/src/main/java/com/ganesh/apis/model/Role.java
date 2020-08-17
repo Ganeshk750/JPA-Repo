1a34c5bc99df39a74acdc8727cd0eb0f423db740
Ganeshk750 <ganesh9ot@gmail.com>
2020-08-17 19:51:25 +0530

    $ adding two model class with there mapping

--- many-to-many-uni/src/main/java/com/ganesh/apis/model/Role.java
+++ many-to-many-uni/src/main/java/com/ganesh/apis/model/Role.java
@@ -1,9 +1,13 @@
 package com.ganesh.apis.model;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="roles")
 public class Role {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
--- many-to-many-uni/src/main/java/com/ganesh/apis/model/User.java
+++ many-to-many-uni/src/main/java/com/ganesh/apis/model/User.java
@@ -1,5 +1,80 @@
 package com.ganesh.apis.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String moblie;
+	@Column(unique = true)
+	private String email;
+	
+	@ManyToMany(targetEntity = Role.class, cascade = CascadeType.ALL)
+	private List<Role> roles;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getMoblie() {
+		return moblie;
+	}
+
+	public void setMoblie(String moblie) {
+		this.moblie = moblie;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+	
+	
 
 }
